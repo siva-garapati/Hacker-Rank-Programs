dc4749d3d311d3177a4f0c32473b62a73e2be9ef
siva-garapati <113223516+siva-garapati@users.noreply.github.com>
2025-03-20 10:45:54 +0530

    Create Find Digits.py
    Link - https://www.hackerrank.com/challenges/find-digits/problem

---
+++ Find Digits.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    # Write your code here
+    c=0
+    y=n
+    while n!=0:
+        d=n%10
+        if d>0:
+            if y%d==0:
+                c+=1
+        n=n//10
+    return c
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
